45d875393238364f5e06d2f8015a96b4d57c43e3
elirob <elirob@ceh.ac.uk>
2017-05-08 13:43:33 +0100

    Update worldmap.js

--- worldmap.js
+++ worldmap.js
@@ -4,14 +4,14 @@
   <title>jVectorMap demo</title>
   <link rel="stylesheet" href="jquery-jvectormap.css" type="text/css" media="screen"/>
   <script src="jquery.js"></script>
-  <script src="jquery-jvectormap-js"></script>
-  <script src="jquery-jvectormap-world.js"></script>
+  <script src="https://github.com/bjornd/jvectormap/blob/master/jquery-jvectormap.js"></script>
+  <script src="http://jvectormap.com/js/jquery-jvectormap-continents-mill.js"></script>
 </head>
 <body>
   <div id="world-map" style="width: 600px; height: 400px"></div>
   <script>
     $(function(){
-      $('#world-map').vectorMap({map: 'world_mill'});
+      $('#world-map').vectorMap({map: 'continents-mill'});
     });
   </script>
 </body>
